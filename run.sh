511dc673fddcfd450ab962772bfc5e904b9352ab
Manuel Maximiliano E2ETRAVELSOLUTIONS <141150930+mmanuele2etravelsolutions@users.noreply.github.com>
2025-09-01 12:43:15 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo docker run -d \
-  --name redis_server \
+  --name redis_server_8.2.1 \
   --restart always \
-  -p 6379:6379 \
+  -p 6380:6379 \
   -v /var/lib/redis:/data \
-  redis:7-alpine \
+  redis:8.2.1-alpine3.22 \
   redis-server \
-  --requirepass XXXXXXXXXX \
-  --bind 0.0.0.0
+    --requirepass "XXXXXXXXXX" \
+    --bind 0.0.0.0 \
+    --appendonly yes
